1b0a1353363176b64f453370fa8b530fa86198d5
Matteo Banerjee <matteobanerjee@gmail.com>
2020-08-17 10:27:00 -0400

    feat: add an option to suppress phone validation for load testing

--- src/lib/phone-format.js
+++ src/lib/phone-format.js
@@ -1,5 +1,7 @@
 import { PhoneNumberUtil, PhoneNumberFormat } from "google-libphonenumber";
 
+const skipValidation = process.env.SUPPRESS_PHONE_VALIDATION === "1";
+
 export const getFormattedPhoneNumber = (cell, country = "US") => {
   const phoneUtil = PhoneNumberUtil.getInstance();
   // we return an empty string vs null when the phone number is inValid
@@ -7,6 +9,10 @@ export const getFormattedPhoneNumber = (cell, country = "US") => {
   // then when contacts have cell.length < 12 (+1), it's deleted before assignments are created
   try {
     const inputNumber = phoneUtil.parse(cell, country);
+
+    if (skipValidation && phoneUtil.isPossibleNumber(inputNumber)) {
+      return phoneUtil.format(inputNumber, PhoneNumberFormat.E164);
+    }
     const isValid = phoneUtil.isValidNumber(inputNumber);
     if (isValid) {
       return phoneUtil.format(inputNumber, PhoneNumberFormat.E164);
