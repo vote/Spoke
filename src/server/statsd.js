241f65eb1727dbd8fe05c91269b26cbb88344e2e
Matteo Banerjee <matteobanerjee@gmail.com>
2020-08-26 19:37:20 -0400

    feat: report messages sent to datadog

---
+++ src/server/statsd.js
@@ -0,0 +1,22 @@
+import { config } from "../config";
+import hotShots from "hot-shots";
+
+const isEnabled = !!(config.DD_AGENT_HOST && config.DD_DOGSTATSD_PORT)
+
+let client;
+
+const getClient = () => {
+  if (isEnabled && !client) {
+    client = new hotShots.StatsD({
+      host: config.DD_AGENT_HOST,
+      port: config.DD_DOGSTATSD_PORT,
+      globalTags: config.DD_TAGS.split(",")
+    });
+  }
+  return client;
+}
+
+export default {
+  isEnabled,
+  getClient,
+}
