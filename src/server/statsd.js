19ee787718c2fa939b2431e2a6f3b65d76cd8422
Matteo Banerjee <matteobanerjee@gmail.com>
2020-08-29 16:59:18 -0400

    chore: debug datadog custom metrics (revert me)

--- src/server/statsd.js
+++ src/server/statsd.js
@@ -1,7 +1,7 @@
-import { config } from "../config";
 import hotShots from "hot-shots";
+import { config } from "../config";
 
-const isEnabled = !!(config.DD_AGENT_HOST && config.DD_DOGSTATSD_PORT)
+const isEnabled = !!(config.DD_AGENT_HOST && config.DD_DOGSTATSD_PORT);
 
 let client;
 
@@ -13,10 +13,21 @@ const getClient = () => {
       globalTags: config.DD_TAGS.split(",")
     });
   }
+
   return client;
-}
+};
+
+// experiment, revert if it's not needed
+const scopedClient = () => {
+  if (isEnabled) {
+    return getClient().childClient({
+      prefix: "spoke_custom."
+    });
+  }
+};
 
 export default {
   isEnabled,
   getClient,
-}
+  scopedClient
+};
