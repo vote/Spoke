a82e6c4180443ca2fd4e9378f853e71a78c24e29
Matteo Banerjee <matteobanerjee@gmail.com>
2020-10-08 14:25:08 -0400

    Switchboard outage

--- dev-tools/replay-failed-messages-0923.js
+++ dev-tools/replay-failed-messages-0923.js
@@ -6,42 +6,54 @@ import { messageComponents } from "../src/server/api/lib/message-sending";
 /* tslint:disable */
 
 const pool = new Pool({ connectionString: process.env.SPOKE_DATABASE_URL });
-const numbersClient = new NumbersClient({
-  apiKey: process.env.SWITCHBOARD_API_KEY,
-  endpointBaseUrl: process.env.SWITCHBOARD_BASE_URL
-});
 
-const start = new Date()
+const start = new Date();
 const summary = async () => {
   console.log("END", (new Date() - start) / 1000);
   const { rows: result } = await pool.query(`
     SELECT COUNT(*)
-    FROM public.message
-    INNER JOIN ad_hoc.failed_messages_0923 fm
-      ON (message.contact_number = fm.contact_number)
-    WHERE message.created_at >= '2020-09-23T00:00:00Z'
-      AND message.created_at < '2020-09-24T02:00:00Z'
-      AND message.send_status = 'SENT'
-      AND NOT fm.retried
-      AND message.num_media > 0
+    from message
+             join campaign_contact on campaign_contact.id = message.campaign_contact_id
+             join campaign on campaign_contact.campaign_id = campaign.id
+             join messaging_service_stick on messaging_service_stick.cell = message.contact_number
+        and messaging_service_stick.organization_id = campaign.organization_id
+             JOIN messaging_service
+                  ON messaging_service.messaging_service_sid = messaging_service_stick.messaging_service_sid
+    WHERE message.created_at >= '2020-10-03T12:00:00Z'
+      AND message.created_at <= '2020-10-05T16:00:00Z'
+      AND message.send_status = 'ERROR'
       AND NOT message.is_from_contact
+      AND NOT campaign_contact.is_opted_out
+      AND service_id = ''
+      AND messaging_service.account_sid IN (
+                                            'https://switchboard2.voteamericaspoke.com',
+                                            'https://switchboard3.voteamericaspoke.com',
+                                            'https://switchboard4.voteamericaspoke.com',
+                                            'https://switchboard5.voteamericaspoke.com',
+                                            'https://switchboard6.voteamericaspoke.com',
+                                            'https://switchboard7.voteamericaspoke.com',
+                                            'https://switchboard8.voteamericaspoke.com',
+                                            'https://switchboard9.voteamericaspoke.com'
+        )
+      AND campaign_id NOT IN (255, 256, 257, 258, 259)
   `);
 
   return result;
 };
 
-const BATCH_SIZE = 290000;
-const CONCURRENCY = 10;
+const BATCH_SIZE = 1500000;
+const CONCURRENCY = 100;
 
 const resendMessage = async (
   id,
   text,
   contactNumber,
   profileId,
-  contactZipCode
+  contactZipCode,
+  accountSid
 ) => {
   try {
-    console.log(`SENDING [${id}]: contact number ${contactNumber}`);
+    console.log(`SENDING [${id}] [${accountSid}]: contact number ${contactNumber}`);
     const { body, mediaUrl } = messageComponents(text);
     const mediaUrls = mediaUrl ? [mediaUrl] : undefined;
     const messageInput = {
@@ -52,13 +64,13 @@ const resendMessage = async (
       contactZipCode: contactZipCode === "" ? null : contactZipCode
     };
 
-    await pool.query(
-      "update ad_hoc.failed_messages_0923 set retried = true where contact_number = $1",
-      [contactNumber]
-    );
+    const numbersClient = new NumbersClient({
+      apiKey: process.env.SWITCHBOARD_API_KEY,
+      endpointBaseUrl: accountSid
+    });
 
     const sent = await numbersClient.sms.sendMessage(messageInput);
-    console.log(`RESPONSE [${id}]: ${JSON.stringify(sent)}`);
+    console.log(`RESPONSE [${id}] [${accountSid}]: ${JSON.stringify(sent)}`);
 
     const serviceId = sent.data.sendMessage.outboundMessage.id;
 
@@ -67,7 +79,7 @@ const resendMessage = async (
       ["SENT", serviceId, id]
     );
   } catch (ex) {
-    console.log(`ERROR [${id}]`, ex);
+    console.log(`ERROR [${id}] [${accountSid}]`, ex);
     console.log(text);
   }
 };
@@ -81,24 +93,33 @@ const rerun = async timezone => {
       message.text,
       message.contact_number,
       messaging_service_stick.messaging_service_sid,
-      campaign_contact.zip
+      campaign_contact.zip,
+      messaging_service.account_sid
     from message
     join campaign_contact on campaign_contact.id = message.campaign_contact_id
     join campaign on campaign_contact.campaign_id = campaign.id
     join messaging_service_stick on messaging_service_stick.cell = message.contact_number
       and messaging_service_stick.organization_id = campaign.organization_id
-    INNER JOIN ad_hoc.failed_messages_0923 fm
-      ON (message.contact_number = fm.contact_number)
-    WHERE message.created_at >= '2020-09-23T00:00:00Z'
-      AND message.created_at < '2020-09-24T02:00:00Z'
-      AND message.send_status = 'SENT'
-      AND NOT fm.retried
-      AND message.num_media > 0
+    JOIN messaging_service
+         ON messaging_service.messaging_service_sid = messaging_service_stick.messaging_service_sid
+    WHERE message.created_at >= '2020-10-03T12:00:00Z'
+      AND message.created_at <= '2020-10-05T16:00:00Z'
+      AND message.send_status = 'ERROR'
       AND NOT message.is_from_contact
-      -- AND campaign.id IN (2, 43, 5, 49)
       AND NOT campaign_contact.is_opted_out
-       -- and coalesce(campaign_contact.timezone, campaign.timezone) = $2
-    order by campaign.id
+      AND service_id = ''
+      AND messaging_service.account_sid IN (
+          'https://switchboard2.voteamericaspoke.com',
+          'https://switchboard3.voteamericaspoke.com',
+          'https://switchboard4.voteamericaspoke.com',
+          'https://switchboard5.voteamericaspoke.com',
+          'https://switchboard6.voteamericaspoke.com',
+          'https://switchboard7.voteamericaspoke.com',
+          'https://switchboard8.voteamericaspoke.com',
+          'https://switchboard9.voteamericaspoke.com'
+      )
+     AND campaign_id NOT IN (255, 256, 257, 258, 259)
+    ORDER BY message.created_at ASC
     limit $1
   `,
     [BATCH_SIZE] // p, timezone]
@@ -119,7 +140,8 @@ const rerun = async timezone => {
           message.text,
           message.contact_number,
           message.messaging_service_sid,
-          message.zip
+          message.zip,
+          message.account_sid
         )
       )
     );
@@ -132,6 +154,7 @@ const rerun = async timezone => {
   // update service id and send status
 };
 
-rerun("America/New_York")
+summary()
+  .then(() => rerun("America/New_York"))
   .then(summary)
   .then(console.log);
